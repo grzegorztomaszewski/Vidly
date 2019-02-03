6f66b3449e08d33779d41726eb31297520a3c7b9
grzegorztomaszewski <aroxd3@gmail.com>
2019-02-03 23:14:38 +0100

    comment

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css", //added lumen bootstrap
+                      "~/Content/bootstrap-lumen.css", //added lumen bootstrapp
                       "~/Content/site.css"));
         }
     }
